08f869f0e2e4e815971701eafc47d9a4a20a284f
Vasilii Perfilev <vasily.perfilyev@gmail.com>
2021-12-02 12:17:59 -0800

    Add-input-form

---
+++ script.js
@@ -0,0 +1,14 @@
+library = [];
+
+document.querySelector(".btn.addBook").addEventListener('click', showBookForm);
+
+function Book(title, author, pages, isFinished){
+    this.title = title;
+    this. author = author;
+    this.pages = pages;
+    this.isFinished = isFinished;
+}
+
+function showBookForm(){
+    
+}
