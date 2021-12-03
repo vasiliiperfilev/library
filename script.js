ea84c0d9af8b2f610d41b43237c0793c89052a3c
Vasilii Perfilev <vasily.perfilyev@gmail.com>
2021-12-02 22:19:57 -0800

    Add book create event

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ const overlay = document.querySelector(".overlay");
 const bookForm = document.querySelector(".bookForm");
 
 document.querySelector(".btn.addBook").addEventListener('click', showBookForm);
+document.querySelector(".btn.submit").addEventListener('click', createBook);
 overlay.addEventListener('click', closeBookForm)
 bookForm.addEventListener('click', (e) => e.stopPropagation());
 
 function Book(title, author, pages, isFinished){
     this.title = title;
-    this. author = author;
+    this.author = author;
     this.pages = pages;
     this.isFinished = isFinished;
 }
@@ -21,6 +22,14 @@ function showBookForm(){
 }
 
 function closeBookForm() {
+    if (this != overlay) return;
     overlay.style.display = 'none';
     bookForm.style.display = 'none';
+}
+
+function createBook() {
+    const inputs = document.querySelectorAll(".bookForm input");
+    const inputValues = [];
+    inputs.forEach((input) => inputValues.push(input.value))
+    const newBook = new Book(...inputValues);
 }
